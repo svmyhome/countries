c5b3ca7d644ba6ce502f57f4317dab4b43e9f286
Vladimir Sarychev <vladimir.sarychev@lenta.com>
2026-03-15 12:49:50 +0300

    Soap initial

--- src/main/java/my/com/countries/config/ApiConfig.java
+++ src/main/java/my/com/countries/config/ApiConfig.java
@@ -2,15 +2,29 @@ package my.com.countries.config;
 
 import my.com.countries.service.CountryErrorAttributes;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
 
 @Configuration
+@EnableWs
 public class ApiConfig {
+
+    public static final String SOAP_NAMESPACE = "countries";
 
     @Value("${api.version}")
     private String apiVersion;
+
+    @Value("${countries.base-uri}")
+    private String countriesBaseUri;
 
     @Bean
 
@@ -18,5 +32,29 @@ public class ApiConfig {
         return new CountryErrorAttributes(
                 apiVersion
         );
+    }
+
+
+    @Bean
+    public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext applicationContext) {
+        final MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean<>(servlet, "/ws/*");
+    }
+
+    @Bean(name = "countries")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countriesSchema) {
+        final DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("CountriesPort");
+        wsdl11Definition.setLocationUri(countriesBaseUri + "/ws");
+        wsdl11Definition.setTargetNamespace(SOAP_NAMESPACE);
+        wsdl11Definition.setSchema(countriesSchema);
+        return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema countriesSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("countries.xsd"));
     }
 }
---
+++ src/main/java/my/com/countries/service/soap/CountriesEndpoint.java
@@ -0,0 +1,71 @@
+package my.com.countries.service.soap;
+
+
+
+import static my.com.countries.config.ApiConfig.SOAP_NAMESPACE;
+import my.com.countries.CountriesResponse;
+import my.com.countries.Country;
+import my.com.countries.CountryResponse;
+import my.com.countries.IdCountryRequest;
+import my.com.countries.PageCountryRequest;
+import my.com.countries.domain.graphql.CountryGql;
+import my.com.countries.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class CountriesEndpoint {
+
+    private final CountryService countryService;
+
+    @Autowired
+    public CountriesEndpoint(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @PayloadRoot(namespace = SOAP_NAMESPACE, localPart = "idCountryRequest")
+    @ResponsePayload
+    public CountryResponse country(@RequestPayload IdCountryRequest request) {
+        final CountryGql country = countryService.countyGqlById(request.getId());
+        CountryResponse countryResponse = new CountryResponse();
+        Country xmlCountry = new Country();
+        xmlCountry.setId(country.id().toString());
+        xmlCountry.setName(country.name());
+        xmlCountry.setCode(country.code());
+        xmlCountry.setCoordinates(country.coordinates());
+        countryResponse.setCountry(xmlCountry);
+        return countryResponse;
+    }
+
+    @PayloadRoot(namespace = SOAP_NAMESPACE, localPart = "pageCountryRequest")
+    @ResponsePayload
+    public CountriesResponse countries(@RequestPayload PageCountryRequest request) {
+        Page<CountryGql> country = (Page<CountryGql>) countryService.allCountriesGql(
+                PageRequest.of(
+                        request.getPage(),
+                        request.getSize()
+                )
+        );
+        CountriesResponse countriesResponse = new CountriesResponse();
+        countriesResponse.setTotalPages(country.getTotalPages());
+        countriesResponse.setTotalElements(country.getTotalElements());
+        countriesResponse.getCountries().addAll(
+                country.getContent().stream().map(
+                        gqlCountry -> {
+                            Country xmlCountry = new Country();
+                            xmlCountry.setId(xmlCountry.getId());
+                            xmlCountry.setName(xmlCountry.getName());
+                            xmlCountry.setCode(xmlCountry.getCode());
+                            xmlCountry.setCoordinates(xmlCountry.getCoordinates());
+                            return xmlCountry;
+                        }
+                ).toList()
+        );
+        return countriesResponse;
+    }
+}
